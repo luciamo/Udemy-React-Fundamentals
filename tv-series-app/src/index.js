34cd43485178277430002df60095b8ad8df490fe
Luciabo <lucianoeduardo@live.com>
2018-05-19 11:39:06 -0300

    Lesson 2 - react elements

---
+++ tv-series-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const greeting = React.createElement('h1', {}, 'Hello World!');
+
+ReactDOM.render(greeting, document.getElementById('root'));
+registerServiceWorker();
