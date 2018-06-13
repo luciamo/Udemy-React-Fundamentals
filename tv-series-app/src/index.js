4dd11153699cc0c42cfc6c275844c46fdc736c90
Luciano Eduardo Dantas Bezerra Filho <luciano.eduardo@imd.local>
2018-06-13 10:17:39 -0300

    Lesson 7 - Lifecycle

--- tv-series-app/src/index.js
+++ tv-series-app/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const greeting = React.createElement('h1', {}, 'Hello World!');
-
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
